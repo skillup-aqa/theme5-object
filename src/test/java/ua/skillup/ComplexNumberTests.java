5a316552743fa1a554ddb0280fb0701be426edc5
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-04-01 12:33:38 +0200

    Adding tests for equals and hashCode to ComplexNumber and IntArray

--- src/test/java/ua/skillup/ComplexNumberTests.java
+++ src/test/java/ua/skillup/ComplexNumberTests.java
@@ -2,14 +2,17 @@ package ua.skillup;
 
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+
 public class ComplexNumberTests {
     @Test
     public void testAdd() {
         ComplexNumber a = new ComplexNumber(1, 2);
         ComplexNumber b = new ComplexNumber(3, 4);
         ComplexNumber c = a.add(b);
-        assert c.getReal() == 4;
-        assert c.getImaginary() == 6;
+        assertEquals(c.getReal(), 4);
+        assertEquals(c.getImaginary(), 6);
     }
 
     @Test
@@ -17,8 +20,8 @@ public class ComplexNumberTests {
         ComplexNumber a = new ComplexNumber(1, 2);
         ComplexNumber b = new ComplexNumber(3, 4);
         ComplexNumber c = a.subtract(b);
-        assert c.getReal() == -2;
-        assert c.getImaginary() == -2;
+        assertEquals(c.getReal(), -2);
+        assertEquals(c.getImaginary(), -2);
     }
 
     @Test
@@ -26,8 +29,8 @@ public class ComplexNumberTests {
         ComplexNumber a = new ComplexNumber(1, 2);
         ComplexNumber b = new ComplexNumber(3, 4);
         ComplexNumber c = a.multiply(b);
-        assert c.getReal() == -5;
-        assert c.getImaginary() == 10;
+        assertEquals(c.getReal(), -5);
+        assertEquals(c.getImaginary(), 10);
     }
 
     @Test
@@ -35,37 +38,65 @@ public class ComplexNumberTests {
         ComplexNumber a = new ComplexNumber(1, 2);
         ComplexNumber b = new ComplexNumber(3, 4);
         ComplexNumber c = a.divide(b);
-        assert c.getReal() == 0.44;
-        assert c.getImaginary() == 0.08;
+        assertEquals(c.getReal(), 0.44);
+        assertEquals(c.getImaginary(), 0.08);
     }
 
     @Test
     public void testConjugate() {
         ComplexNumber a = new ComplexNumber(1, 2);
         ComplexNumber b = a.conjugate();
-        assert b.getReal() == 1;
-        assert b.getImaginary() == -2;
+        assertEquals(b.getReal(), 1);
+        assertEquals(b.getImaginary(), -2);
     }
 
     @Test
     public void testAbs() {
         ComplexNumber a = new ComplexNumber(3, 4);
-        assert a.abs() == 5;
+        assertEquals(a.abs(), 5);
     }
 
     @Test
     public void testMultiplyByFactor() {
         ComplexNumber a = new ComplexNumber(1, 2);
         ComplexNumber b = a.multiply(2);
-        assert b.getReal() == 2;
-        assert b.getImaginary() == 4;
+        assertEquals(b.getReal(), 2);
+        assertEquals(b.getImaginary(), 4);
     }
 
     @Test
     public void testDivideByFactor() {
         ComplexNumber a = new ComplexNumber(1, 2);
         ComplexNumber b = a.divide(2);
-        assert b.getReal() == 0.5;
-        assert b.getImaginary() == 1;
+        assertEquals(b.getReal(), 0.5);
+        assertEquals(b.getImaginary(), 1);
+    }
+
+    @Test
+    public void testEquals() {
+        ComplexNumber a = new ComplexNumber(1, 2);
+        ComplexNumber b = new ComplexNumber(1, 2);
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void testNotEquals() {
+        ComplexNumber a = new ComplexNumber(1, 2);
+        ComplexNumber b = new ComplexNumber(2, 1);
+        assertNotEquals(a, b);
+    }
+
+    @Test
+    public void testEqualHashCodes() {
+        ComplexNumber a = new ComplexNumber(1, 2);
+        ComplexNumber b = new ComplexNumber(1, 2);
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    public void testNotEqualHashCodes() {
+        ComplexNumber a = new ComplexNumber(1, 2);
+        ComplexNumber b = new ComplexNumber(2, 1);
+        assertNotEquals(a.hashCode(), b.hashCode());
     }
 }
